60cf121cd155ba0fee84ecdcb65169597d221243
HyLin <linerwang1997@gmail.com>
2023-12-25 22:17:55 +0000

    package support

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='akb_distance',
+    version='0.1',
+    packages=find_packages(),
+    license='Your License',
+    long_description=open('README.md').read(),
+    install_requires=[
+        
+    ],
+)
